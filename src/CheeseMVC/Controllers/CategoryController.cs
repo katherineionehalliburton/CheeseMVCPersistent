cd6b6ae360c1a64df2a9c9bfa5656407ebd9b849
Katherine Halliburton <katherineionehalliburton@gmail.com>
2018-03-29 17:23:44 -0400

    2nd commit - added CategoryController to Controllers directory

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
