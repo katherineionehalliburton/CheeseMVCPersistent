8b1343bd1ab006608b045b31e60b11d608fa4300
Katherine Halliburton <katherineionehalliburton@gmail.com>
2018-03-31 14:06:49 -0400

    14th commit - set up view/viewmodel/controller to handle Menus

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            var Menus = context.Menu.ToList();
+
+            return View("/Index");
+        }
+    }
+}
